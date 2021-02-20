a9952ad50e40b03e6e006fe158b0a89edd6dfc55
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-02-20 13:06:05 -0600

    progress

--- index.js
+++
@@ -1,9 +0,0 @@
-WebAssembly.instantiateStreaming(fetch('math.wasm')).then(m => {
-  const sum = m.instance.exports.add(19, 3);
-  console.log('sum =', sum);
-  document.getElementById('sum').textContent = sum;
-
-  const distance = m.instance.exports.distance3(1, 2, 4, 6);
-  console.log('distance =', distance);
-  document.getElementById('distance').textContent = distance;
-});
--- rust_example/src/lib.rs
+++
@@ -1,6 +0,0 @@
-#[no_mangle]
-extern "C" fn sum(a: i32, b: i32) -> i32 {
-    let s = a + b;
-    println!("From WASM: Sum is: {:?}", s);
-    s
-}
