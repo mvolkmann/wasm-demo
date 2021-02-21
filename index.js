7a3789395c71ddaa9dff2acd780b5e9e03349ad5
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2021-02-21 16:53:54 -0600

    added calling JS functions from Rust

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+function cube(n) {
+  return n ** 3;
+}
+
+function square(n) {
+  return n * n;
+}
+
+const importObject = {env: {cube, square}};
+
 //const wasmPath = 'math.wasm';
 const wasmPath = 'rust-math/target/wasm32-unknown-unknown/debug/rust_math.wasm';
-WebAssembly.instantiateStreaming(fetch(wasmPath)).then(m => {
-  const {distance, distance2, distance3, sum} = m.instance.exports;
+WebAssembly.instantiateStreaming(fetch(wasmPath), importObject).then(m => {
+  const {
+    distance,
+    distance2,
+    distance3,
+    sum,
+    sum_of_square_and_cube
+  } = m.instance.exports;
   document.getElementById('sum').textContent = sum(19, 3);
   document.getElementById('distance').textContent = distance(2, 3, 5, 7);
   console.log('distance1 =', distance(2, 3, 5, 7));
-  console.log('distance2 =', distance2(2, 3, 5, 7));
-  console.log('distance3 =', distance3(2, 3, 5, 7));
+  //console.log('distance2 =', distance2(2, 3, 5, 7));
+  //console.log('distance3 =', distance3(2, 3, 5, 7));
+  console.log('**2 + **3 =', sum_of_square_and_cube(2));
 });
--- rust-math/src/lib.rs
+++ rust-math/src/lib.rs
@@ -1,3 +1,8 @@
+extern "C" {
+    fn cube(n: f64) -> f64;
+    fn square(n: f64) -> f64;
+}
+
 #[no_mangle]
 pub fn sum(n1: f64, n2: f64) -> f64 {
     n1 + n2
@@ -6,4 +11,13 @@ pub fn sum(n1: f64, n2: f64) -> f64 {
 #[no_mangle]
 pub fn distance(x1: f64, y1: f64, x2: f64, y2: f64) -> f64 {
     ((x1 - x2).powi(2) + (y1 - y2).powi(2)).sqrt()
+}
+
+#[no_mangle]
+pub fn sum_of_square_and_cube(n: f64) -> f64 {
+    let result;
+    unsafe {
+        result = square(n) + cube(n);
+    }
+    result
 }
